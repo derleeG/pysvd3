4473d9b6370a771de147e6d63ead587f697df319
derleeG <derleewang@gmail.com>
2018-09-13 17:40:49 -0400

    expose qr and pd

--- src/svd3.pyx
+++ src/svd3.pyx
@@ -2,10 +2,22 @@
 
 import numpy as np
 cimport numpy as np
+cimport cython
 np.import_array()
 
 
 cdef extern from '../lib/svd3/svd3.h':
+    void QRDecomposition(\
+            float b11, float b12, float b13, \
+            float b21, float b22, float b23, \
+            float b31, float b32, float b33, \
+            float &q11, float &q12, float &q13, \
+            float &q21, float &q22, float &q23, \
+            float &q31, float &q32, float &q33, \
+            float &r11, float &r12, float &r13, \
+            float &r21, float &r22, float &r23, \
+            float &r31, float &r32, float &r33)
+
     void svd(\
             float a11, float a12, float a13, \
             float a21, float a22, float a23, \
@@ -20,38 +32,86 @@ cdef extern from '../lib/svd3/svd3.h':
             float &v21, float &v22, float &v23, \
             float &v31, float &v32, float &v33)
 
+    void pd(\
+            float a11, float a12, float a13, \
+            float a21, float a22, float a23, \
+            float a31, float a32, float a33, \
+            float &u11, float &u12, float &u13, \
+            float &u21, float &u22, float &u23, \
+            float &u31, float &u32, float &u33, \
+            float &p11, float &p12, float &p13, \
+            float &p21, float &p22, float &p23, \
+            float &p31, float &p32, float &p33)
+
+
+def qr3(np.ndarray[float, ndim=2, mode='c'] A not None):
+    if A.shape[0] != 3 or A.shape[1] != 3:
+        raise ValueError("Only 3x3 matrix supported")
+
+    cdef np.ndarray[float, ndim=2, mode='c'] Q = np.zeros((3,3), dtype=np.float32)
+    cdef np.ndarray[float, ndim=2, mode='c'] R = np.zeros((3,3), dtype=np.float32)
+
+    QRDecomposition(
+        A[0, 0], A[0, 1], A[0, 2],
+        A[1, 0], A[1, 1], A[1, 2],
+        A[2, 0], A[2, 1], A[2, 2],
+        Q[0, 0], Q[0, 1], Q[0, 2],
+        Q[1, 0], Q[1, 1], Q[1, 2],
+        Q[2, 0], Q[2, 1], Q[2, 2],
+        R[0, 0], R[0, 1], R[0, 2],
+        R[1, 0], R[1, 1], R[1, 2],
+        R[2, 0], R[2, 1], R[2, 2])
+
+    R[1, 0] = 0
+    R[2, 0] = 0
+    R[2, 1] = 0
+
+    return Q, R
+
 
 def svd3(np.ndarray[float, ndim=2, mode='c'] A not None):
-    assert A.shape[0] == 3 and A.shape[1] == 3
+    if A.shape[0] != 3 or A.shape[1] != 3:
+        raise ValueError("Only 3x3 matrix supported")
 
-    # prepare output buffer
-    U = np.zeros((3, 3), dtype=float)
-    S = np.zeros((3, 3), dtype=float)
-    V = np.zeros((3, 3), dtype=float)
-
-    cdef float u11, u12, u13, u21, u22, u23, u31, u32, u33
-    cdef float s11, s12, s13, s21, s22, s23, s31, s32, s33
-    cdef float v11, v12, v13, v21, v22, v23, v31, v32, v33
+    cdef np.ndarray[float, ndim=2, mode='c'] U = np.zeros((3,3), dtype=np.float32)
+    cdef np.ndarray[float, ndim=2, mode='c'] S = np.zeros((3,3), dtype=np.float32)
+    cdef np.ndarray[float, ndim=2, mode='c'] V = np.zeros((3,3), dtype=np.float32)
 
     svd(A[0, 0], A[0, 1], A[0, 2],
         A[1, 0], A[1, 1], A[1, 2],
         A[2, 0], A[2, 1], A[2, 2],
-        u11, u12, u13,
-        u21, u22, u23,
-        u31, u32, u33,
-        s11, s12, s13,
-        s21, s22, s23,
-        s31, s32, s33,
-        v11, v12, v13,
-        v21, v22, v23,
-        v31, v32, v33)
+        U[0, 0], U[0, 1], U[0, 2],
+        U[1, 0], U[1, 1], U[1, 2],
+        U[2, 0], U[2, 1], U[2, 2],
+        S[0, 0], S[1, 1], S[2, 2],
+        S[1, 0], S[0, 1], S[1, 2],
+        S[2, 0], S[2, 1], S[0, 2],
+        V[0, 0], V[1, 0], V[2, 0],
+        V[0, 1], V[1, 1], V[2, 1],
+        V[0, 2], V[1, 2], V[2, 2])
 
-    U = np.array([[u11, u12, u13], [u21, u22, u23], [u31, u32, u33]])
-    #S = np.array([[s11, s12, s13], [s21, s22, s23], [s31, s32, s33]])
-    S = np.array([s11, s22, s33])
-    Vh = np.array([[v11, v21, v31], [v12, v22, v32], [v13, v23, v33]])
+    return U, S[0, :], V
 
-    return U, S, Vh
+
+def pd3(np.ndarray[float, ndim=2, mode='c'] A not None):
+    if A.shape[0] != 3 or A.shape[1] != 3:
+        raise ValueError("Only 3x3 matrix supported")
+
+    cdef np.ndarray[float, ndim=2, mode='c'] U = np.zeros((3,3), dtype=np.float32)
+    cdef np.ndarray[float, ndim=2, mode='c'] P = np.zeros((3,3), dtype=np.float32)
+
+    pd(
+        A[0, 0], A[0, 1], A[0, 2],
+        A[1, 0], A[1, 1], A[1, 2],
+        A[2, 0], A[2, 1], A[2, 2],
+        U[0, 0], U[0, 1], U[0, 2],
+        U[1, 0], U[1, 1], U[1, 2],
+        U[2, 0], U[2, 1], U[2, 2],
+        P[0, 0], P[0, 1], P[0, 2],
+        P[1, 0], P[1, 1], P[1, 2],
+        P[2, 0], P[2, 1], P[2, 2])
+
+    return U, P
 
 
 
--- src/unit_test.py
+++ src/unit_test.py
@@ -1,93 +1,113 @@
 import numpy as np
+from timeit import default_timer as timer
 from svd3 import svd3 as svd
+from svd3 import qr3 as qr
+from svd3 import pd3 as pd
 
 
-def test_svd_correctness():
 
-    A = np.random.rand(3, 3).astype(np.float32)
-    print('fast svd3 implementation')
-    U, S, Vh = svd(A)
-    print('matrix A:')
-    print(A)
-    print('matrix U:')
-    print(U)
-    print('matrix S:')
-    print(S)
-    print('matrix Vh:')
-    print(Vh)
-    print('matrix USVt:')
-    USVt = np.matmul(U * S, Vh)
-    print(USVt)
-    print('USVt - A')
-    print(USVt - A)
+def get_func(method):
+    if method == 'qr':
+        func = qr
+        stat_func = lambda a, q, r: (a, q, r, np.matmul(q, r), \
+                np.linalg.norm(np.matmul(q, r) - a))
+        print_fmt = 'matrix A: \n {}\nmatrix Q:\n {}\nmatrix R:\n {}\n' \
+                + 'matrix QR:\n {}\nerror: {}'
 
-    print('np.linalg.svd implementation')
-    U, S, Vh = np.linalg.svd(A)
-    print('matrix A:')
-    print(A)
-    print('matrix U:')
-    print(U)
-    print('matrix S:')
-    print(S)
-    print('matrix Vh:')
-    print(Vh)
-    print('matrix USVt:')
-    USVt = np.matmul(U * S, Vh)
-    print(USVt)
-    print('USVt - A')
-    print(USVt - A)
+        return func, stat_func, print_fmt
+
+    elif method == 'svd':
+        func = svd
+        stat_func = lambda a, u, s, vh: (a, u, s, vh, np.matmul(u*s, vh), \
+                np.linalg.norm(np.matmul(u*s, vh) - a))
+        print_fmt = 'matrix A: \n {}\nmatrix U:\n {}\nmatrix S:\n {}\n' \
+                + 'matrix Vh:\n {}\nmatrix USV*:\n {}\nerror: {}'
+
+        return func, stat_func, print_fmt
+
+    elif method == 'pd':
+        func = pd
+        stat_func = lambda a, u, p: (a, u, p, np.matmul(u, p), \
+                np.linalg.norm(np.matmul(u, p) - a))
+        print_fmt = 'matrix A: \n {}\nmatrix U:\n {}\nmatrix P:\n {}\n' \
+                + 'matrix UP:\n {}\nerror: {}'
+
+        return func, stat_func, print_fmt
 
 
-def benchmark_accuracy():
+def test_correctness(func, stat_func, print_fmt, data=None):
+    if data is None:
+        data = np.random.rand(3, 3).astype(np.float32)
 
-    N = 100000
-    a = np.random.rand(N, 3, 3).astype(np.float32)
+    out = func(data)
+    out = stat_func(data, *out)
+    print(print_fmt.format(*out))
+
+    return data
+
+
+def benchmark_accuracy(func, stat_func, data=None):
+    if data is None:
+        N = 100000
+        data = np.random.rand(N, 3, 3).astype(np.float32)
+    else:
+        N = data.shape[0]
 
     err_sum = 0
-    for A in a:
-        U, S, Vh = svd(A)
-        USVt = np.matmul(U * S, Vh)
-        err_sum += np.linalg.norm(USVt - A)
+    for A in data:
+        out = func(A)
+        out = stat_func(A, *out)
+        err_sum += out[-1]
 
-    print('fast svd3')
-    print('Average error (||USV* - A||F) over {} random samples: {}'.format(N, err_sum/N))
+    print('Average error over {} random samples: {}'.format(N, err_sum/N))
 
-    err_sum = 0
-    for A in a:
-        U, S, Vh = np.linalg.svd(A)
-        USVt = np.matmul(U * S, Vh)
-        err_sum += np.linalg.norm(USVt - A)
-
-    print('np linalg')
-    print('Average error (||USV* - A||F) over {} random samples: {}'.format(N, err_sum/N))
+    return data
 
 
-def benchmark_speed():
-
-    from timeit import default_timer as timer
-
-    N = 100000
-    a = np.random.rand(N, 3, 3).astype(np.float32)
+def benchmark_speed(func, data=None):
+    if data is None:
+        N = 100000
+        data = np.random.rand(N, 3, 3).astype(np.float32)
+    else:
+        N = data.shape[0]
 
     start = timer()
-    for A in a:
-        _, _, _ = svd(A)
+    for A in data:
+        _ = func(A)
     end = timer()
 
-    print('fast svd3')
     print('Average execution time over {} random samples: {} ms'.format(N, (end-start)/N*1e6))
 
-    start = timer()
-    for A in a:
-        _, _, _ = np.linalg.svd(A)
-    end = timer()
+    return data
 
-    print('np linalg')
-    print('Average execution time over {} random samples: {} ms'.format(N, (end-start)/N*1e6))
+
+def test_method_correctness(method, data=None):
+    print('Testing method: {}'.format(method))
+    funcs = get_func(method)
+    return test_correctness(*funcs, data)
+
+
+def benchmark_method_accuracy(method, data=None):
+    print('Benchmarking accuracy: {}'.format(method))
+    funcs = get_func(method)
+    return benchmark_accuracy(*funcs[:-1], data)
+
+
+def benchmark_method_speed(method, data=None):
+    print('Benchmarking speed: {}'.format(method))
+    funcs = get_func(method)
+    return benchmark_speed(funcs[0], data)
+
 
 
 if  __name__ == '__main__':
 
-    #test_svd_correctness()
-    benchmark_accuracy()
-    benchmark_speed()
+    test_method_correctness('qr')
+    test_method_correctness('svd')
+    test_method_correctness('pd')
+    benchmark_method_accuracy('qr')
+    benchmark_method_accuracy('svd')
+    benchmark_method_accuracy('pd')
+    benchmark_method_speed('qr')
+    benchmark_method_speed('svd')
+    benchmark_method_speed('pd')
