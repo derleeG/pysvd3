2f6de80841b79238dfbb0d33af1f379c1ca19a09
derleeG <derleewang@gmail.com>
2018-09-15 00:06:21 -0400

    revise unit testing

--- src/unit_test.py
+++ src/unit_test.py
@@ -3,129 +3,121 @@ import scipy.linalg
 from timeit import default_timer as timer
 import svd3
 
-
+N = 100000
 def get_func(method):
     if method == 'qr':
         func = svd3.qr3
-        stat_func = lambda a, q, r: (a, q, r, np.matmul(q, r), \
-                np.linalg.norm(np.matmul(q, r) - a))
+        stat_func = lambda a, o: (a, *o, np.matmul(o[0], o[1]), \
+                np.linalg.norm(np.matmul(o[0], o[1]) - a)/\
+                np.linalg.norm(a))
+        data_func = lambda x: (np.random.randn(x, 3, 3).astype(np.float32),)
         print_fmt = 'matrix A: \n {}\nmatrix Q:\n {}\nmatrix R:\n {}\n' \
                 + 'matrix QR:\n {}\nerror: {}'
-
-        return func, stat_func, print_fmt
 
     elif method == 'np_qr':
         func = np.linalg.qr
-        stat_func = lambda a, q, r: (a, q, r, np.matmul(q, r), \
-                np.linalg.norm(np.matmul(q, r) - a))
+        stat_func = lambda a, o: (a, *o, np.matmul(o[0], o[1]), \
+                np.linalg.norm(np.matmul(o[0], o[1]) - a)/\
+                np.linalg.norm(a))
+        data_func = lambda x: (np.random.randn(x, 3, 3).astype(np.float32),)
         print_fmt = 'matrix A: \n {}\nmatrix Q:\n {}\nmatrix R:\n {}\n' \
                 + 'matrix QR:\n {}\nerror: {}'
 
-        return func, stat_func, print_fmt
-
     elif method == 'svd':
         func = svd3.svd3
-        stat_func = lambda a, u, s, vh: (a, u, s, vh, np.matmul(u*s, vh), \
-                np.linalg.norm(np.matmul(u*s, vh) - a))
+        stat_func = lambda a, o: (a, *o, np.matmul(o[0]*o[1], o[2]), \
+                np.linalg.norm(np.matmul(o[0]*o[1], o[2]) - a)/\
+                np.linalg.norm(a))
+        data_func = lambda x: (np.random.randn(x, 3, 3).astype(np.float32),)
         print_fmt = 'matrix A: \n {}\nmatrix U:\n {}\nmatrix S:\n {}\n' \
                 + 'matrix Vh:\n {}\nmatrix USV*:\n {}\nerror: {}'
-
-        return func, stat_func, print_fmt
 
     elif method == 'np_svd':
         func = np.linalg.svd
-        stat_func = lambda a, u, s, vh: (a, u, s, vh, np.matmul(u*s, vh), \
-                np.linalg.norm(np.matmul(u*s, vh) - a))
+        stat_func = lambda a, o: (a, *o, np.matmul(o[0]*o[1], o[2]), \
+                np.linalg.norm(np.matmul(o[0]*o[1], o[2]) - a)/\
+                np.linalg.norm(a))
+        data_func = lambda x: (np.random.randn(x, 3, 3).astype(np.float32),)
         print_fmt = 'matrix A: \n {}\nmatrix U:\n {}\nmatrix S:\n {}\n' \
                 + 'matrix Vh:\n {}\nmatrix USV*:\n {}\nerror: {}'
 
-        return func, stat_func, print_fmt
-
     elif method == 'pd':
         func = svd3.pd3
-        stat_func = lambda a, u, p: (a, u, p, np.matmul(u, p), \
-                np.linalg.norm(np.matmul(u, p) - a))
+        stat_func = lambda a, o: (a, *o, np.matmul(o[0], o[1]), \
+                np.linalg.norm(np.matmul(o[0], o[1]) - a)/\
+                np.linalg.norm(a))
+        data_func = lambda x: (np.random.randn(x, 3, 3).astype(np.float32),)
         print_fmt = 'matrix A: \n {}\nmatrix U:\n {}\nmatrix P:\n {}\n' \
                 + 'matrix UP:\n {}\nerror: {}'
-
-        return func, stat_func, print_fmt
 
     elif method == 'np_pd':
         func = scipy.linalg.polar
-        stat_func = lambda a, u, p: (a, u, p, np.matmul(u, p), \
-                np.linalg.norm(np.matmul(u, p) - a))
+        stat_func = lambda a, o: (a, *o, np.matmul(o[0], o[1]), \
+                np.linalg.norm(np.matmul(o[0], o[1]) - a)/\
+                np.linalg.norm(a))
+        data_func = lambda x: (np.random.randn(x, 3, 3).astype(np.float32),)
         print_fmt = 'matrix A: \n {}\nmatrix U:\n {}\nmatrix P:\n {}\n' \
                 + 'matrix UP:\n {}\nerror: {}'
-
-        return func, stat_func, print_fmt
 
     else:
         error('Unknown method: {}'.format(method))
 
+    return data_func, func, stat_func, print_fmt
 
-def test_correctness(func, stat_func, print_fmt, data=None):
-    if data is None:
-        data = np.random.rand(3, 3).astype(np.float32)
+def test_correctness(data_func, func, stat_func, print_fmt):
 
-    out = func(data)
-    out = stat_func(data, *out)
-    print(print_fmt.format(*out))
-
-    return data
+    datas = data_func(1)
+    for data in zip(*datas):
+        out = stat_func(*data, func(*data))
+        print(print_fmt.format(*out))
+    return
 
 
-def benchmark_accuracy(func, stat_func, data=None):
-    if data is None:
-        N = 100000
-        data = np.random.rand(N, 3, 3).astype(np.float32)
-    else:
-        N = data.shape[0]
+def benchmark_accuracy(data_func, func, stat_func):
 
+    datas = data_func(N)
     err_sum = 0
-    for A in data:
-        out = func(A)
-        out = stat_func(A, *out)
-        err_sum += out[-1]
+    for data in zip(*datas):
+        err_sum += stat_func(*data, func(*data))[-1]
 
     print('Average error over {} random samples: {}'.format(N, err_sum/N))
 
-    return data
+    return
 
 
-def benchmark_speed(func, data=None):
-    if data is None:
-        N = 100000
-        data = np.random.rand(N, 3, 3).astype(np.float32)
-    else:
-        N = data.shape[0]
+def benchmark_speed(data_func, func):
+
+    datas = data_func(N)
 
     start = timer()
-    for A in data:
-        _ = func(A)
+    for data in zip(*datas):
+        _ = func(*data)
     end = timer()
 
     print('Average execution time over {} random samples: {} us'.format(N, (end-start)/N*1e6))
 
-    return data
+    return
 
 
-def test_method_correctness(method, data=None):
+def test_method_correctness(method):
     print('Testing method: {}'.format(method))
     funcs = get_func(method)
-    return test_correctness(*funcs, data)
+    test_correctness(*funcs)
+    return
 
 
-def benchmark_method_accuracy(method, data=None):
+def benchmark_method_accuracy(method):
     print('Benchmarking accuracy: {}'.format(method))
     funcs = get_func(method)
-    return benchmark_accuracy(*funcs[:-1], data)
+    benchmark_accuracy(*funcs[:-1])
+    return
 
 
-def benchmark_method_speed(method, data=None):
+def benchmark_method_speed(method):
     print('Benchmarking speed: {}'.format(method))
     funcs = get_func(method)
-    return benchmark_speed(funcs[0], data)
-
+    benchmark_speed(*funcs[:-2])
+    return
 
 
 if  __name__ == '__main__':
@@ -133,17 +125,15 @@ if  __name__ == '__main__':
     #test_method_correctness('qr')
     #test_method_correctness('svd')
     #test_method_correctness('pd')
-    N = 100000
-    data = np.random.rand(N, 3, 3).astype(np.float32)
-    benchmark_method_accuracy('qr', data)
-    benchmark_method_accuracy('np_qr', data)
-    benchmark_method_accuracy('svd', data)
-    benchmark_method_accuracy('np_svd', data)
-    benchmark_method_accuracy('pd', data)
-    benchmark_method_accuracy('np_pd', data)
-    benchmark_method_speed('qr', data)
-    benchmark_method_speed('np_qr', data)
-    benchmark_method_speed('svd', data)
-    benchmark_method_speed('np_svd', data)
-    benchmark_method_speed('pd', data)
-    benchmark_method_speed('np_pd', data)
+    benchmark_method_accuracy('qr')
+    benchmark_method_accuracy('np_qr')
+    benchmark_method_accuracy('svd')
+    benchmark_method_accuracy('np_svd')
+    benchmark_method_accuracy('pd')
+    benchmark_method_accuracy('np_pd')
+    benchmark_method_speed('qr')
+    benchmark_method_speed('np_qr')
+    benchmark_method_speed('svd')
+    benchmark_method_speed('np_svd')
+    benchmark_method_speed('pd')
+    benchmark_method_speed('np_pd')
