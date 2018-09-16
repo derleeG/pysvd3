86273325cc2e7c0e1f759155da3db0fa5d3ed9cc
derleeG <derleewang@gmail.com>
2018-09-15 22:58:49 -0400

    adjust parameter

--- src/patch_rsqrt.sh
+++ src/patch_rsqrt.sh
@@ -1,5 +1,5 @@
 if [ ! -f lib/svd3/svd3.h.bak ]; then
     echo "Patching lib/svd3/svd3.h to improve accuracy"
-    sed -i.bak ' s/for (int i=0;i<4;i++)/for (int i=0; i<5;i++)/g; /inline float rsqrt(float x) {/a \
+    sed -i.bak 's/return x \* rsqrt1(x);/return x \* rsqrt(x);/g; s/for (int i=0;i<4;i++)/for (int i=0; i<5;i++)/g; /inline float rsqrt(float x) {/a \
         return 1/sqrt(x);' lib/svd3/svd3.h
 fi
